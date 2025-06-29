555aed7a7e827b663b1f1e8a4f5ad314d1ce3b6a
ABo0oO0D <Abdallaosman1972005@gmail.com>
2025-06-29 23:02:54 +0800

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-// A struct to hold one record of the dataset
+// Data record
 struct Data {
     long key;
     string value;
 };
 
-// Merge the two sorted ranges [first, mid) and [mid, last)
+// Merge two sorted halves [first,mid) and [mid,last)
 template<typename It, typename Comp>
 void merge_range(It first, It mid, It last, Comp comp) {
     vector<typename It::value_type> buffer;
@@ -28,10 +28,11 @@ void merge_range(It first, It mid, It last, Comp comp) {
     }
     buffer.insert(buffer.end(), i, mid);
     buffer.insert(buffer.end(), j, last);
+
     move(buffer.begin(), buffer.end(), first);
 }
 
-// Recursive merge-sort over [first,last)
+// Recursive merge sort
 template<typename It, typename Comp>
 void merge_sort(It first, It last, Comp comp) {
     auto len = distance(first, last);
@@ -47,53 +48,67 @@ int main(int argc, char* argv[]) {
     cin.tie(nullptr);
 
     if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <dataset.csv>\n";
+        cerr << "Usage: " << argv[0] << " <dataset_n.csv>\n";
         return 1;
     }
 
-    // 1) Open the input file
-    ifstream in{argv[1]};
+    string infile = argv[1];
+    // Extract the N from dataset_N.csv
+    auto uscore = infile.rfind('_');
+    auto dot     = infile.rfind('.');
+    if (uscore == string::npos || dot == string::npos || uscore >= dot) {
+        cerr << "Error: input filename must be like dataset_N.csv\n";
+        return 1;
+    }
+    string N = infile.substr(uscore+1, dot - (uscore+1));
+
+    // Construct output filename
+    string outfile = "merge_sort_" + N + ".csv";
+
+    // 1) Read input
+    ifstream in{infile};
     if (!in) {
-        cerr << "Error: could not open \"" << argv[1] << "\"\n";
+        cerr << "Error: could not open \"" << infile << "\"\n";
         return 1;
     }
-
-    // 2) Read and parse each line "number,value"
     vector<Data> vec;
     string line;
     while (getline(in, line)) {
         if (line.empty()) continue;
-        auto sep = line.find(',');          // look for comma now
+        auto sep = line.find(',');            // now splitting on comma
         if (sep == string::npos) continue;
-
-        string numPart = line.substr(0, sep);
-        string txtPart = line.substr(sep + 1);
         try {
             Data d;
-            d.key   = stol(numPart);
-            d.value = move(txtPart);
+            d.key   = stol(line.substr(0, sep));
+            d.value = line.substr(sep+1);
             vec.push_back(move(d));
         } catch (...) {
-            // skip bad lines or non-numeric keys
+            continue;
         }
     }
+    in.close();
 
-    // 3) Time the merge_sort
+    // 2) Sort & time
     auto t0 = chrono::high_resolution_clock::now();
     merge_sort(vec.begin(), vec.end(),
                [](auto &a, auto &b){ return a.key < b.key; });
     auto t1 = chrono::high_resolution_clock::now();
-
     double elapsed_ms =
         chrono::duration<double, milli>(t1 - t0).count();
 
-    // 4) Report timing (ms) to stderr
-    cerr << elapsed_ms << "\n";
-
-    // 5) Output sorted data to stdout as "number,value"
-    for (auto &d : vec) {
-        cout << d.key << "," << d.value << "\n";
+    // 3) Write sorted output
+    ofstream out{outfile};
+    if (!out) {
+        cerr << "Error: could not create \"" << outfile << "\"\n";
+        return 1;
     }
+    for (auto &d : vec) {
+        out << d.key << "," << d.value << "\n";
+    }
+    out.close();
+
+    // 4) Print running time
+    cout << elapsed_ms << "\n";
 
     return 0;
 }
