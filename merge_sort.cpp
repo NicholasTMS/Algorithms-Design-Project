3b1ebcc58342a03fa33b3f9954ed8db00328e8ab
ABo0oO0D <Abdallaosman1972005@gmail.com>
2025-06-29 22:51:43 +0800

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,11 +1,10 @@
-// merge_sort_timed.cpp
+// merge_sort.cpp
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <iterator>
 #include <chrono>
 
 using namespace std;
@@ -17,35 +16,29 @@ struct Data {
 };
 
 // Merge the two sorted ranges [first, mid) and [mid, last)
-template<typename RandomIt, typename Compare>
-void merge_range(RandomIt first, RandomIt mid, RandomIt last, Compare comp) {
-    vector<typename RandomIt::value_type> buffer;
+template<typename It, typename Comp>
+void merge_range(It first, It mid, It last, Comp comp) {
+    vector<typename It::value_type> buffer;
     buffer.reserve(distance(first, last));
 
-    RandomIt i = first, j = mid;
+    It i = first, j = mid;
     while (i != mid && j != last) {
-        if (comp(*i, *j))
-            buffer.push_back(*i++);
-        else
-            buffer.push_back(*j++);
+        if (comp(*i, *j)) buffer.push_back(*i++);
+        else               buffer.push_back(*j++);
     }
-
-    // copy any leftovers
     buffer.insert(buffer.end(), i, mid);
     buffer.insert(buffer.end(), j, last);
-
-    // move back into original range
     move(buffer.begin(), buffer.end(), first);
 }
 
-// Classic recursive merge-sort over [first, last)
-template<typename RandomIt, typename Compare>
-void merge_sort(RandomIt first, RandomIt last, Compare comp) {
+// Recursive merge-sort over [first,last)
+template<typename It, typename Comp>
+void merge_sort(It first, It last, Comp comp) {
     auto len = distance(first, last);
-    if (len <= 1) return;
-    RandomIt mid = first + len/2;
+    if (len < 2) return;
+    It mid = first + len/2;
     merge_sort(first, mid, comp);
-    merge_sort(mid, last, comp);
+    merge_sort(mid,   last, comp);
     merge_range(first, mid, last, comp);
 }
 
@@ -59,19 +52,20 @@ int main(int argc, char* argv[]) {
     }
 
     // 1) Open the input file
-    ifstream in(argv[1]);
+    ifstream in{argv[1]};
     if (!in) {
         cerr << "Error: could not open \"" << argv[1] << "\"\n";
         return 1;
     }
 
-    // 2) Read and parse each line "number/value"
+    // 2) Read and parse each line "number,value"
     vector<Data> vec;
     string line;
     while (getline(in, line)) {
         if (line.empty()) continue;
-        auto sep = line.find('/');
-        if (sep == string::npos) continue;  // skip bad lines
+        auto sep = line.find(',');          // look for comma now
+        if (sep == string::npos) continue;
+
         string numPart = line.substr(0, sep);
         string txtPart = line.substr(sep + 1);
         try {
@@ -79,28 +73,26 @@ int main(int argc, char* argv[]) {
             d.key   = stol(numPart);
             d.value = move(txtPart);
             vec.push_back(move(d));
-        } catch (const exception&) {
-            // skip lines where stol() fails
-            continue;
+        } catch (...) {
+            // skip bad lines or non-numeric keys
         }
     }
 
     // 3) Time the merge_sort
     auto t0 = chrono::high_resolution_clock::now();
     merge_sort(vec.begin(), vec.end(),
-               [](const Data &a, const Data &b){ return a.key < b.key; });
+               [](auto &a, auto &b){ return a.key < b.key; });
     auto t1 = chrono::high_resolution_clock::now();
 
     double elapsed_ms =
         chrono::duration<double, milli>(t1 - t0).count();
 
-    // 4) Report timing to stderr
-    cerr << "Merge sort elapsed time: "
-         << elapsed_ms << " ms\n";
+    // 4) Report timing (ms) to stderr
+    cerr << elapsed_ms << "\n";
 
-    // 5) Output sorted data to stdout
+    // 5) Output sorted data to stdout as "number,value"
     for (auto &d : vec) {
-        cout << d.key << "/" << d.value << "\n";
+        cout << d.key << "," << d.value << "\n";
     }
 
     return 0;
